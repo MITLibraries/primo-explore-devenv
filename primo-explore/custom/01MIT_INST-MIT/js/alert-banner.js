cda10ff4a1e33de29c8022c03398860a767e5e4c
Adam Shire <adamshire123@gmail.com>
2024-12-17 07:59:06 -0700

    add internal toggle for alert banner.
    set toggle to off.

--- primo-explore/custom/01MIT_INST-MIT/js/alert-banner.js
+++ primo-explore/custom/01MIT_INST-MIT/js/alert-banner.js
@@ -1,6 +1,9 @@
 var alertBannerTemplate =
-    '<div class="full-width-alert">How are we doing? <a href="https://mit.co1.qualtrics.com/jfe/form/SV_0HZvFmPYRjCzSCO">Give us feedback on your search experience</a>.</div>';
+    '<div ng-if="$ctrl.active" class="full-width-alert">How are we doing? <a href="https://mit.co1.qualtrics.com/jfe/form/SV_0HZvFmPYRjCzSCO">Give us feedback on your search experience</a>.</div>';
 
 app.component('mitAlertBanner', {
+    controller: function () {
+        this.active = false; //show banner if `true`
+    },
     template: alertBannerTemplate
 })
--- primo-explore/custom/01MIT_INST-MIT/js/custom.js
+++ primo-explore/custom/01MIT_INST-MIT/js/custom.js
@@ -23,9 +23,12 @@ app.component("prmNoSearchResultAfter", {
     template: '<mit-no-search-result></mit-no-search-result>'
 });
 
-var alertBannerTemplate = '<div class="full-width-alert">How are we doing? <a href="https://mit.co1.qualtrics.com/jfe/form/SV_0HZvFmPYRjCzSCO">Give us feedback on your search experience</a>.</div>';
+var alertBannerTemplate = '<div ng-if="$ctrl.active" class="full-width-alert">How are we doing? <a href="https://mit.co1.qualtrics.com/jfe/form/SV_0HZvFmPYRjCzSCO">Give us feedback on your search experience</a>.</div>';
 
 app.component('mitAlertBanner', {
+    controller: function controller() {
+        this.active = false; //show banner if `true`
+    },
     template: alertBannerTemplate
 });
 app.component("mitAskUs", {
