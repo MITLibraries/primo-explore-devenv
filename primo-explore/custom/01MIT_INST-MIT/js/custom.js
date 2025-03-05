09b05bcac64791cd5ed6c6627720b510339cf3f0
Adam Shire <adamshire123@gmail.com>
2025-03-05 17:08:28 -0500

    update concatenated custom.js file

--- primo-explore/custom/01MIT_INST-MIT/js/custom.js
+++ primo-explore/custom/01MIT_INST-MIT/js/custom.js
@@ -112,7 +112,7 @@ _mtm.push({ 'mtm.startTime': new Date().getTime(), 'event': 'mtm.Start' });
     var d = document,
         g = d.createElement('script'),
         s = d.getElementsByTagName('script')[0];
-    g.async = true;g.src = 'https://matomo.libraries.mit.edu/js/container_SF5ZTPZP_staging_3fa37b5dc1fa04ca2a73a555.js';s.parentNode.insertBefore(g, s);
+    g.async = true;g.src = 'https://matomo.libraries.mit.edu/js/container_SF5ZTPZP.js';s.parentNode.insertBefore(g, s);
 })();
 
 /* End Matomo Tag Manager */
