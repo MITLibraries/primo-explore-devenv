c45a9c8d58852d585a9aec9e5b99b86fb7908142
Adam Shire <adamshire123@gmail.com>
2025-06-27 10:41:12 -0400

    No results page updates
    Why these changes are being introduced:
    * due to the recent change to Primo where the default search includes the CDI, it seemed some tweaks were in order to better reflect that.
    
    How this addresses that need:
    * updates language and adds additional links
    
    Side effects of this change:
    * none
    
    Relevant ticket(s):
    * https://mitlibraries.atlassian.net/browse/ES-3226

--- primo-explore/custom/01MIT_INST-MIT/js/no-result.js
+++ primo-explore/custom/01MIT_INST-MIT/js/no-result.js
@@ -1,4 +1,5 @@
-var noResultTemplate = `<md-card class="default-card zero-margin _md md-primoExplore-theme">
+var noResultTemplate = `
+<md-card class="default-card zero-margin _md md-primoExplore-theme">
             <md-card-title>
                 <md-card-title-text>
                     <span class="md-headline">Oops, no records found! Let's keep digging...</span>
@@ -6,18 +7,33 @@ var noResultTemplate = `<md-card class="default-card zero-margin _md md-primoExp
             </md-card-title>
             <md-card-content>
                 <p><span class="bold-text">No results matching "{{$ctrl.getSearchTerm}}". Is the spelling correct?</span></p>
-                <p><span >More options:</span></p>
+                <p><span>More options:</span></p>
                     <ul>
-                        <li>Articles: Select the "Search in full text" checkbox in the sidebar, or request via <a href="https://libraries.mit.edu/illiad">ILB/ILLiad</a>.</li>
-                        <li>If you have not already logged in, doing so may retrieve more results.</li>
-                        <li>Books, physical materials: Request via BorrowDirect/InterLibrary Borrowing (ILB) by finding the item in <a href="https://mit.on.worldcat.org/search?queryString={{$ctrl.getSearchTerm}}" data-content-target="https://mit.on.worldcat.org/search?queryString={{$ctrl.getSearchTerm}}">WorldCat</a>.</li>
-                        <li>Archives and manuscripts: Search and request via <a href="http://archivesspace.mit.edu/">ArchivesSpace</a>.</li>
-                        <li><a href="https://libraries.mit.edu/suggest-purchase">Suggest a purchase</a>.</li>
-                        <li><a href="https://libraries.mit.edu/ask">Ask Us</a> for more help!</li>
+                        <li>Get more results by:</li>
+                        <ul>
+                            <li> Selecting "Also search in text of articles" in the sidebar </li>
+                            <li> Signing in (some content only available to authenticated users) </li>
+                        </ul>
+                        <li>Articles: Request via <a href="https://libraries.mit.edu/illiad">ILB/ILLiad.</a></li>
+                        <li>Books, physical materials: Request via BorrowDirect/InterLibrary Borrowing (ILB) by finding the item in <a href="https://mit.on.worldcat.org/search?queryString={{$ctrl.getSearchTerm}}">WorldCat</a>.</li>
+                        <li>Try these other specialized search tools:</li>
+                        <ul>
+                            <li><a href="https://libguides.mit.edu/az.php">A-Z Databases</a>: Browse or search for databases.</li>
+                            <li><a href="https://archnet.org/">Archnet</a>: Architecture, design, and conservation issues related to the Muslim world.</li>
+                            <li><a href="http://archivesspace.mit.edu/">ArchivesSpace</a>: Archives and manuscripts in our <a href="https://libraries.mit.edu/distinctive-collections">Distinctive Collections</a>.</li>
+                            <li><a href="https://libraries.mit.edu/dspace">DSpace@MIT</a>: MIT theses, articles and other research materials.</li>
+                            <li><a href="https://libraries.mit.edu/dome">Dome</a>: MIT Libraries’ digital collections – images, media, maps, and more.</li>
+                            <li><a href="https://geodata.libraries.mit.edu/">GeoData</a>: GIS/spatial data available at MIT.</li>
+                            <li><a href="https://libraries.mit.edu/experts">Subject guides</a>: Created by our librarians for targeted research by subject.</li>
+                        </ul>
                     </ul>
+                    <p><span>You may also <a href="https://libraries.mit.edu/suggest-purchase">Suggest a purchase</a> or 
+                    <a href="https://libraries.mit.edu/ask">Ask Us</a> for more help!
+                    </span></p>
             </md-card-content>
         </md-card>
         `
+
 
 app.component("mitNoSearchResult", {
     //require the controller for the prmNoSearchResult directive
